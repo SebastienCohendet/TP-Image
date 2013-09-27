ec4bd3c363b4128504d598ad0333cb41ddb42717
Sebastien COHENDET <Sebastien.cohendet@centraliens-nantes.net>
2013-09-27 17:23:32 +0200

    Working file reading

---
+++ src/File.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class File {
+    //lecture du fichier texte	
+    
+   public String chaine="";
+   protected String fichier ="lena.pgm";
+   
+   public File() {
+        try{
+                InputStream ips=new FileInputStream(fichier); 
+                InputStreamReader ipsr=new InputStreamReader(ips);
+                BufferedReader br=new BufferedReader(ipsr);
+                String ligne;
+                while ((ligne=br.readLine())!=null){
+                        //System.out.println(ligne);
+                        chaine+=ligne+"\n";
+                }
+                br.close(); 
+        }		
+        catch (Exception e){
+                System.out.println(e.toString());
+        }
+   }
+}
---
+++ src/TPImage.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author Sébastien
+ */
+
+import java.io.*;
+
+public class TPImage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       File fichier = new File();
+       
+       System.out.println(fichier.chaine);
+     
+	}
+   
+}
